24033686d28ddb0bf22088b06a157c95278a2f01
pranay2102 <43007881+pranay2102@users.noreply.github.com>
2024-10-21 18:27:29 +0530

    Update test-script-to-gen-code-scanning-alerts.js
    Added vulnerabilities

--- scripts/test-script-to-gen-code-scanning-alerts.js
+++ scripts/test-script-to-gen-code-scanning-alerts.js
@@ -2,3 +2,8 @@ function vulnerableFunction() {
   var userInput = prompt("Enter a number:");
   eval("alert('User input: ' + userInput);"); // CodeQL might flag this for using eval
 }
+
+function xssVulnerable() {
+    var userInput = prompt("Enter some text:");
+    document.body.innerHTML += "<div>" + userInput + "</div>"; // Potential XSS
+}
