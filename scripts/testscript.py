e44e210f39412aa123502cc045c1044c26b29128
pranay2102 <pranayram.madhavaram@gmail.com>
2024-10-22 17:39:54 +0530

    new script : testscript.py

---
+++ scripts/testscript.py
@@ -0,0 +1,6 @@
+curl --location --request GET 'https://api.getpostman.com/collections' \
+        --header 'X-Api-Key: ffjghtghutgjfbgfyufbgfhbgf8fgfgfu9'
+
+POSTMAN_API="jfuhrrguirhgur8rbhrbh"
+
+echo "$POSTMAN_API"
