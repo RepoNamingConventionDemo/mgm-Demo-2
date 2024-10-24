8750cbb0b9a192509bbb555c439bf0a1cb9a626f
pranay2102 <43007881+pranay2102@users.noreply.github.com>
2024-10-24 20:56:53 +0530

    Delete xyz.py

--- xyz.py
+++
@@ -1,6 +0,0 @@
-# Fake Azure Storage Account Key to trigger secret scanning
-DefaultEndpointsProtocol=https;AccountName=fakestorageaccount;AccountKey=1q2w3e4r5t6y7u8i9o0pEXAMPLEKEY1234567890qwertyuiop
-
-
-adafruit_io_key="uruehrguier9erugbeg8bvhdfv"
-	databricks_access_token="ireviehrv8rvneuveruvenrurvneurr8"
